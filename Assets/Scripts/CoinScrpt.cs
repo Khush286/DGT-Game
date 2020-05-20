6f2281842ca5c2fe1e364783bb8ab86609743e3a
Khush286 <62322943+Khush286@users.noreply.github.com>
2020-05-20 14:19:19 +1200

    may 20

---
+++ Assets/Scripts/CoinScrpt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScrpt : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        CoinTextScript.coinAmount += 1;
+        Destroy(gameObject);
+    }
+}
