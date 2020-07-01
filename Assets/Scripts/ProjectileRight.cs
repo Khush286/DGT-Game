100026871c6b698e91af3b4f2d79ac24b48ebf7b
Khush286 <62322943+Khush286@users.noreply.github.com>
2020-07-02 09:29:29 +1200

    2.5 july

--- Assets/Scripts/ProjectileRight.cs
+++ Assets/Scripts/ProjectileRight.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /****************************** Project Header ******************************\
-Script Name:  ProjectileUp
+Script Name:  ProjectileRight
 Project:      DGT-Game Dungeon Runner
 Author:       Khushwant Singh
 
